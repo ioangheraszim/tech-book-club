0e39b5a3f1dd6719dab698a30193fbfc4bb0be67
ioangheraszim <gijohny17@gmail.com>
2025-09-02 16:21:12 +0300

    style: added CSS resets and linked _reset.scss to main.scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import Hero from './components/Hero/Hero'
+
 function App() {
   return (
     <>
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,9 @@
+// import styles from './Hero.module.scss'
+
+export default function Hero() {
+  return (
+    <>
+      <section>Hello World!2x</section>
+    </>
+  )
+}
